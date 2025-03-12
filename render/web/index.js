e1114cd68cc50d2ef64e0c5843fb9586d071e300
FrogOfJuly <kirill.golubev@utu.fi>
2025-03-13 01:39:06 +0200

    fixes

--- render/web/index.js
+++ render/web/index.js
@@ -32,8 +32,8 @@ await init();
 
 const canvas = document.getElementById("cursive-wasm-canvas");
 canvas.style.display = "block";
-canvas.setAttribute("width", "1000");
-canvas.setAttribute("height", "1000");
+// canvas.setAttribute("width", "1000");
+// canvas.setAttribute("height", "1000");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 // Add a rectangle at (10, 10) with size 100x100 pixels
