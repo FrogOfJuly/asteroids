681bcb8a7015a5ff33dda1847797add94de0c584
FrogOfJuly <kirill.golubev@utu.fi>
2025-03-13 15:37:32 +0200

    cleanup

--- eframe_template/src/main.rs
+++ eframe_template/src/main.rs
@@ -19,7 +19,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(eframe_template::AsteroidApp::new(cc)))),
     )
 }
 
@@ -49,7 +49,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(eframe_template::AsteroidApp::new(cc)))),
             )
             .await;
 
